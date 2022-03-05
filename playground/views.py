ff3615b72dc2de975af088ce212a1043f9761040
djabirpc <djabir.pc@outlook.fr>
2022-03-05 20:55:04 +0100

    Part1.5

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.db.models import Q, F
-from store.models import Product,Order
+from django.db import connection
+from store.models import Collection, Order, OrderItem, Product
 
-# Create your views here.
 def say_hello(request):
-    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    return render(request,'hello.html',{'name':'Djabir', 'orders':list(query_set)})
+    with connection.cursor() as cursor:
+        cursor.callproc('get_customers',[1, 2, 'a'])
+    return render(request,'hello.html',{'name':'Djabir', 'result': list(queryset)})
