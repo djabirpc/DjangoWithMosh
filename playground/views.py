2a8da4a39e22cd06e389387de6f7b604bc748128
djabirpc <djabir.pc@outlook.fr>
2022-03-05 14:41:27 +0100

    Part1.5.14

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-def calculate():
-    x=1
-    y=2
-    return x
+from django.db.models import Q, F
+from store.models import Product,Order
 
 # Create your views here.
 def say_hello(request):
-    x= calculate()
-    return render(request,'hello.html',{'name':''})
+    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    return render(request,'hello.html',{'name':'Djabir', 'orders':list(query_set)})
