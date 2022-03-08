1ac98bad7ddc2a4cea7350445748ab4d5cb26335
djabirpc <djabir.pc@outlook.fr>
2022-03-08 23:38:10 +0100

    Part2.2

--- store/serializers.py
+++ store/serializers.py
@@ -3,23 +3,30 @@ from rest_framework import serializers
 
 from store.models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id','title','products_count']
 
-class ProductSerializer(serializers.Serializer):
-    
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(
-        max_digits=6, 
-        decimal_places=2,
-        source = 'unit_price')
+    products_count = serializers.IntegerField()
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','description','slug','inventory','unit_price','price_with_tax','collection']
+
     price_with_tax = serializers.SerializerMethodField(method_name= 'calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name='collection-detail'
-    )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+    
+    # def create(self, validated_data):
+    #     product = Product(**validated_data)
+    #     product.other = 1
+    #     product.save()
+    #     return product
+
+    # def update(self, instance, validated_data):
+    #     instance.unit_price = validated_data.get('unit_price')
+    #     instance.save()
+    #     return instance
