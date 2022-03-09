4eeadbda82e75dccf62280ad37a934184dbf3105
djabirpc <djabir.pc@outlook.fr>
2022-03-09 01:08:11 +0100

    Part2.3.2 CBV

--- store/views.py
+++ store/views.py
@@ -1,7 +1,6 @@
-from re import search
-from urllib import request
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 
 from django.shortcuts import get_object_or_404
@@ -11,9 +10,9 @@ from .models import Collection, Product
 from .serializers import CollectionSerializer, ProductSerializer
 from store import serializers
 
-@api_view(['GET','POST'])
-def product_list(request):
-    if request.method == 'GET':
+
+class ProductList(APIView):
+    def get(self, request):
         queryset = Product.objects.select_related('collection').all()
         serializer = ProductSerializer(
             queryset, 
@@ -21,24 +20,28 @@ def product_list(request):
             context = {'request':request}
             )
         return Response(serializer.data)
-    elif request.method == 'POST':
+    
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk = id)
-    if request.method == 'GET':
+        
+class ProductDetail(APIView):
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk = id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+    
+    def put(self, request, id):
+        product = get_object_or_404(Product, pk = id)
         serializer = ProductSerializer(product, data = request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk = id)
         if product.orderitems.count() > 0:
             return Response(
                 {'error':'Product cannot be deleted because it is containe order item'},
