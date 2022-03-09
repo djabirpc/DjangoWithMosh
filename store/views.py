dc33554dcce56fcf232f8dd9e907f4b1c0bd3068
djabirpc <djabir.pc@outlook.fr>
2022-03-09 16:54:03 +0100

    Part2.3.11

--- store/views.py
+++ store/views.py
@@ -1,24 +1,22 @@
-from itertools import product
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 
-from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+
 from django.db.models.aggregates import Count
 
+from .filters import ProductFilter
 from .models import Collection, OrderItem, Product, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id = collection_id)
-        return queryset
 
     def get_serializer_context(self):
         return {'request': self.request}
